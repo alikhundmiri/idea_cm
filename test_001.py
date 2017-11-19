44372dc5c3bb3447ede1dc3fe41ccbdf7e3aea6d
Ali Khundmiri <salikhundmiri@gmail.com>
2017-11-19 07:48:17 +0530

    initial commit

---
+++ test_001.py
@@ -0,0 +1,15 @@
+from googleapiclient.discovery import build
+import pprint
+
+my_api_key = "AIzaSyBs-_V2cRaDtt52qiniwukofmqxfpwHCXE"
+my_cse_id = "007758778882673451564:_--q0zt43pa"
+
+def google_search(search_term, api_key, cse_id, **kwargs):
+	service = build("customsearch", "v1", developerKey=api_key)
+	res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+	return res['items']
+
+results = google_search('top 10 tips for watch buying', my_api_key, my_cse_id, num=5)
+
+for result in results:
+	pprint.pprint(result)
